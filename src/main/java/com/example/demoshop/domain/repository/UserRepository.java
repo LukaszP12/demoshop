1a8563edfd12ab707ed5533f523f6ee8af3ccda0
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-09-10 23:51:09 +0200

    user repository

---
+++ src/main/java/com/example/demoshop/domain/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.demoshop.domain.repository;
+
+import com.example.demoshop.domain.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository {
+
+    // Save or update a user
+    User save(User user);
+
+    // Find a user by ID
+    Optional<User> findById(User.UserId id);
+
+    // Optional: find by email
+    Optional<User> findByEmail(String email);
+}
