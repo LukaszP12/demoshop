84c80ceca27091942010ae2cdb31c09b51a46857
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-09-12 09:54:45 +0200

    my catalog domain supports:
    Organizing products into categories
    
    Supporting multiple variations of products
    
    Storing product recommendations

--- src/main/java/com/example/demoshop/domain/model/catalogue/Category.java
+++ src/main/java/com/example/demoshop/domain/model/catalogue/Category.java
@@ -1,30 +1,38 @@
-package com.example.demoshop.domain.model.catalogue;
+package main.java.com.example.demoshop.java.com.example.demoshop.domain.model.catalogue;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+
+@Document(collection = "categories")
 public class Category {
 
+    @Id
+    private String id;
     private final String name;
+    private String parentCategoryId;
 
-    public Category(String name) {
+    public Category(String name, String parentCategoryId) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Category name cannot be empty");
         }
         this.name = name;
+        this.parentCategoryId = parentCategoryId;
     }
 
-    public String name() {
-        return name;
-    }
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getParentCategoryId() { return parentCategoryId; }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Category)) return false;
-        Category category = (Category) o;
-        return name.equals(category.name);
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getId(), category.getId()) && Objects.equals(getName(), category.getName()) && Objects.equals(getParentCategoryId(), category.getParentCategoryId());
     }
 
     @Override
     public int hashCode() {
-        return name.hashCode();
+        return Objects.hash(getId(), getName(), getParentCategoryId());
     }
 }
