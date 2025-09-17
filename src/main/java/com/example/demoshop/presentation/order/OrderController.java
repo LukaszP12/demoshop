86d51b114917b4e1bb78c441415dddb5c471579b
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-09-17 21:48:29 +0200

    # mark order as canceled with corresponding event

--- src/main/java/com/example/demoshop/presentation/order/OrderController.java
+++ src/main/java/com/example/demoshop/presentation/order/OrderController.java
@@ -3,6 +3,7 @@ package main.java.com.example.demoshop.java.com.example.demoshop.presentation.or
 import main.java.com.example.demoshop.java.com.example.demoshop.application.order.OrderWorkflowService;
 import main.java.com.example.demoshop.java.com.example.demoshop.domain.model.order.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +40,12 @@ public class OrderController {
     @PostMapping("/{orderId}/return")
     public ResponseEntity<Order> requestReturn(@PathVariable String orderId) {
         Order updatedOrder = orderService.requestReturn(orderId);
+        return ResponseEntity.ok(updatedOrder);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<Order> cancelOrder(@PathVariable String orderId){
+        Order updatedOrder = orderService.cancelOrder(orderId);
         return ResponseEntity.ok(updatedOrder);
     }
 }
