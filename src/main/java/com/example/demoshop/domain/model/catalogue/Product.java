5099be8744d3e8e53aa614d2ea3392b65e0bb5b2
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-09-19 13:07:24 +0200

    add removal method for Product

--- src/main/java/com/example/demoshop/domain/model/catalogue/Product.java
+++ src/main/java/com/example/demoshop/domain/model/catalogue/Product.java
@@ -20,7 +20,8 @@ public class Product {
     private String description;
     private List<ProductCategory> categories;
 
-    public Product() {}
+    public Product() {
+    }
 
     public Product(String name, String type, String brand, double price, int volume_ml, int stock, String description, List<ProductCategory> categories) {
         this.name = name;
@@ -33,28 +34,78 @@ public class Product {
         this.categories = categories;
     }
 
-    public void addCategory(ProductCategory category){
-        if (!categories.contains(category)){
+    public void addCategory(ProductCategory category) {
+        if (!categories.contains(category)) {
             categories.add(category);
         }
     }
 
-    // Getters and setters
-    public String getId() { return id; }
-    public String getName() { return name; }
-    public String getType() { return type; }
-    public String getBrand() { return brand; }
-    public double getPrice() { return price; }
-    public int getVolume_ml() { return volume_ml; }
-    public int getStock() { return stock; }
-    public String getDescription() { return description; }
+    public void removeCategory(ProductCategory category) {
+        categories.remove(category);
+    }
 
-    public void setId(String id) { this.id = id; }
-    public void setName(String name) { this.name = name; }
-    public void setType(String type) { this.type = type; }
-    public void setBrand(String brand) { this.brand = brand; }
-    public void setPrice(double price) { this.price = price; }
-    public void setVolume_ml(int volume_ml) { this.volume_ml = volume_ml; }
-    public void setStock(int stock) { this.stock = stock; }
-    public void setDescription(String description) { this.description = description; }
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getVolume_ml() {
+        return volume_ml;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setVolume_ml(int volume_ml) {
+        this.volume_ml = volume_ml;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
