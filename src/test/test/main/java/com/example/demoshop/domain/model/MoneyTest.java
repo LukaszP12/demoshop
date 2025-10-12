185514381d20a1c48868d8f5229a51858de900bc
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-12 15:58:22 +0200

    added two tests related to money conversion which will later be implemented

---
+++ src/test/test/main/java/com/example/demoshop/domain/model/MoneyTest.java
@@ -0,0 +1,30 @@
+package main.java.com.example.demoshop.domain.model;
+
+import main.java.com.example.demoshop.java.com.example.demoshop.domain.model.common.Money;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+class MoneyTest {
+
+    @Test
+    void shouldConvertAmountToTargetCurrencyUsingGivenRate() {
+        Money pln = new Money(BigDecimal.valueOf(100.00), Currency.getInstance("PLN"));
+
+        Money eur = pln.convertTo(Currency.getInstance("EUR"), BigDecimal.valueOf(0.22));
+
+        assertEquals(BigDecimal.valueOf(22.00).setScale(2), eur.getAmount());
+        assertEquals("EUR", eur.getCurrency().getCurrencyCode());
+    }
+
+    @Test
+    void shouldRoundToTwoDecimalPlaces() {
+        Money pln = new Money(BigDecimal.valueOf(100.00), Currency.getInstance("PLN"));
+
+        Money usd = pln.convertTo(Currency.getInstance("USD"), BigDecimal.valueOf(0.2567));
+
+        assertEquals(BigDecimal.valueOf(25.67).setScale(2), usd.getAmount());
+    }
+
+}
