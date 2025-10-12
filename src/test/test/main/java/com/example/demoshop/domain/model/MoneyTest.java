cae8c8d8d4e17ed14cd9f878d9ea6d4158a80cb0
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-12 16:14:10 +0200

    test driven functionality to be implemented soon

--- src/test/test/main/java/com/example/demoshop/domain/model/MoneyTest.java
+++ src/test/test/main/java/com/example/demoshop/domain/model/MoneyTest.java
@@ -6,21 +6,23 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.Currency;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MoneyTest {
 
     @Test
     void shouldConvertAmountToTargetCurrencyUsingGivenRate() {
-        Money pln = new Money(BigDecimal.valueOf(100.00), Currency.getInstance("PLN"));
+        Money pln = Money.of(BigDecimal.valueOf(100.00), "" + Currency.getInstance("PLN"));
 
         Money eur = pln.convertTo(Currency.getInstance("EUR"), BigDecimal.valueOf(0.22));
 
         assertEquals(BigDecimal.valueOf(22.00).setScale(2), eur.getAmount());
-        assertEquals("EUR", eur.getCurrency().getCurrencyCode());
+        assertEquals("EUR", eur.getCurrency());
     }
 
     @Test
     void shouldRoundToTwoDecimalPlaces() {
-        Money pln = new Money(BigDecimal.valueOf(100.00), Currency.getInstance("PLN"));
+        Money pln = Money.of(BigDecimal.valueOf(100.00), "" + Currency.getInstance("PLN"));
 
         Money usd = pln.convertTo(Currency.getInstance("USD"), BigDecimal.valueOf(0.2567));
 
